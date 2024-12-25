dde91b086cdec59790e586ba96ee59c671f6bde2
Gouravsen1202 <gouravsen757@gmail.com>
2024-12-25 19:55:06 +0530

    first commit

---
+++ exectract a data.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+struct info
+{
+	string name ="GOurav";
+	int rollno=123;
+	int marks[5]={12,3,43,5,43};
+}p;
+int main(){
+	cout<<p.rollno<<p.name<<p.rollno;
+}
